a8ed4f4108b14dad0b862832ff3e564dab1130bd
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-02-28 22:09:56 +0800

    feat: :sparkles: add basic styling to pokemon cards on homepage

--- src/server/api/routers/pokemon.ts
+++ src/server/api/routers/pokemon.ts
@@ -1,33 +1,42 @@
-import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
-import fetch from "node-fetch";
+import { z } from "zod"
+import { createTRPCRouter, publicProcedure } from "../trpc"
+import { type pokemonAPI } from "../../schemas/pokemon"
+import fetch from "node-fetch"
+import * as _ from "lodash"
 
 const pokemonSchema = z.object({
   image: z.string(),
   name: z.string()
 })
 
-const pokemonAPI = z.object({
-  name: z.string(),
-  sprites: z.object({
-    front_default: z.string()
-  })
-})
-
 const getAllPokemonsOutput = pokemonSchema.array()
+const getTwoRandomPokemons = pokemonSchema.array()
 
 export const pokemonRouter = createTRPCRouter({
   getAllPokemons: publicProcedure
     .output(getAllPokemonsOutput)
     .query(async () => {
       const urls = getPokemonUrls()
-      const res = await Promise.all([
-        ...urls.map(url => fetch(url).then(response => response.json()))
-      ]) as z.infer<typeof pokemonAPI>[]
+      const res = (await Promise.all([
+        ...urls.map((url) => fetch(url).then((response) => response.json()))
+      ])) as z.infer<typeof pokemonAPI>[]
 
-      return res.map(pokemon => ({
-        name: pokemon.name,
-        image: pokemon.sprites.front_default
+      return res.map((pokemon) => ({
+        name: pokemon?.name,
+        image: pokemon.sprites.other?.["official-artwork"]?.front_default
+      }))
+    }),
+  getTwoRandomPokemons: publicProcedure
+    .output(getTwoRandomPokemons)
+    .query(async () => {
+      const urls = getTwoRandomPokemonUrls()
+      const res = (await Promise.all([
+        ...urls.map((url) => fetch(url).then((res) => res.json()))
+      ])) as z.infer<typeof pokemonAPI>[]
+
+      return res.map((pokemon) => ({
+        name: pokemon?.name,
+        image: pokemon.sprites.other?.["official-artwork"]?.front_default
       }))
     })
 })
@@ -37,5 +46,14 @@ function getPokemonUrls() {
   for (let i = 1; i <= 150; i++) {
     pokemonUrls.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
   }
+  return pokemonUrls
+}
+
+function getTwoRandomPokemonUrls() {
+  const pokemonUrls: string[] = []
+  const firstRandomInt = _.random(1, 150)
+  const secondRandomInt = _.random(1, 150)
+  pokemonUrls.push(`https://pokeapi.co/api/v2/pokemon/${firstRandomInt}`)
+  pokemonUrls.push(`https://pokeapi.co/api/v2/pokemon/${secondRandomInt}`)
   return pokemonUrls
 }
