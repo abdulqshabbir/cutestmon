b2a59b4b605e20c9b95fbd3dcf452494d8a08bd2
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-03-08 20:41:06 +0800

    style: :lipstick: minor styling to pokemon card label

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
           content="Vote on which pokemon you think is most pointy!"
         />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex min-h-screen flex-col items-center justify-center gap-16">
         <h1 className="text-5xl font-extralight text-gray-700">
           Which pokemon is more <span className="italic">pointy</span>?
         </h1>
@@ -53,8 +53,10 @@ function PokemonCard({ name, imageUrl, isLoading }: PokemonCardProps) {
     return <SkeletonPokemonCard />
   }
   return (
-    <div className="flex flex-col gap-8">
-      <div className="text-lg text-gray-500">{name}</div>
+    <div className="flex flex-col gap-2">
+      <h2 className="mb-1 flex justify-center text-lg italic text-gray-400">
+        {name}
+      </h2>
       <div className="rounded-3xl bg-gray-100 transition-all hover:scale-105 hover:cursor-pointer hover:bg-gray-200">
         <Image
           src={imageUrl}
