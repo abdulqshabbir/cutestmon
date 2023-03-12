778246bda79053575e97910f3cdda7ecfd21d797
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-03-12 17:32:30 +0800

    style: :lipstick: add styling for pokemon cards on vote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import { type Dispatch, type SetStateAction, useState } from "react"
 import { type NextPage } from "next"
+import Link from "next/link"
 import Image from "next/image"
 import Head from "next/head"
+import { VscRefresh } from "react-icons/vsc"
+import { AiOutlineBarChart } from "react-icons/ai"
 import { trpc } from "../utils/api"
 import DefaultSpinner from "../components/ui/Spinner"
 
 const Home: NextPage = () => {
   const { data, isError, isLoading } =
-    trpc.pokemons.getTwoRandomPokemons.useQuery()
+    trpc.pokemons.getTwoRandomPokemons.useQuery(undefined, {
+      retry: false,
+      refetchOnWindowFocus: false
+    })
+
+  const [hasCastVote, setHasCastVote] = useState(false)
+
   if (isError) {
     return <div>Sorry something went wrong</div>
   }
@@ -19,8 +29,8 @@ const Home: NextPage = () => {
           content="Vote on which pokemon you think is most pointy!"
         />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center sm:gap-16">
-        <h1 className="p-4 text-3xl font-extralight text-gray-700 sm:text-5xl">
+      <main className="flex min-h-screen flex-col items-center justify-center sm:gap-8">
+        <h1 className="p-4 text-3xl font-light text-gray-500 sm:text-5xl">
           Which pokemon is more <span className="italic">pointy</span>?
         </h1>
         <div className="flex flex-col gap-8 sm:flex-row">
@@ -29,14 +39,30 @@ const Home: NextPage = () => {
             imageUrl={data?.[0]?.image}
             isLoading={isLoading}
             id={data?.[0]?.id}
+            setHasCastVote={setHasCastVote}
+            hasCastVote={hasCastVote}
           />
           <PokemonCard
             name={data?.[1]?.name}
             imageUrl={data?.[1]?.image}
             isLoading={isLoading}
             id={data?.[0]?.id}
+            setHasCastVote={setHasCastVote}
+            hasCastVote={hasCastVote}
           />
         </div>
+        {hasCastVote && (
+          <div className=" flex justify-center gap-8">
+            <button className="flex items-center justify-center gap-2 rounded-lg bg-purple-200 py-4 px-8 text-gray-600 transition-all hover:scale-110 ">
+              <span>Vote Again</span>
+              <VscRefresh />
+            </button>
+            <button className=" flex items-center justify-center gap-2 rounded-lg bg-green-200 py-4 px-8 text-gray-600 transition-all hover:scale-110">
+              <Link href="/leaderboard">Show Results</Link>
+              <AiOutlineBarChart />
+            </button>
+          </div>
+        )}
       </main>
     </>
   )
@@ -47,12 +73,37 @@ interface PokemonCardProps {
   imageUrl?: string | undefined
   id: number | undefined
   isLoading: boolean
+  hasCastVote: boolean
+  setHasCastVote: Dispatch<SetStateAction<boolean>>
 }
 
-function PokemonCard({ name, imageUrl, isLoading, id }: PokemonCardProps) {
+function getTailwindClassesOnVote(
+  hasCastVote: boolean,
+  hasVotedForThisPokemon: boolean
+) {
+  let classes = ""
+  if (hasCastVote && !hasVotedForThisPokemon) {
+    classes += "bg-gray-200 opacity-30 hover:pointer-events-none"
+  } else if (hasCastVote && hasVotedForThisPokemon) {
+    classes += "bg-green-200 opacity-30 hover:pointer-events-none"
+  }
+  return classes
+}
+
+function PokemonCard({
+  name,
+  imageUrl,
+  isLoading,
+  id,
+  hasCastVote,
+  setHasCastVote
+}: PokemonCardProps) {
+  const [votedForThisPokemon, setHasVotedForThisPokemon] = useState(false)
   const mutation = trpc.pokemons.voteForPokemonById.useMutation({
     onSuccess: () => {
       console.log("we voted!")
+      setHasCastVote(true)
+      setHasVotedForThisPokemon(true)
     },
     onError: (error) => {
       console.log("We failed :(")
@@ -72,7 +123,12 @@ function PokemonCard({ name, imageUrl, isLoading, id }: PokemonCardProps) {
       <h2 className="mb-1 flex justify-center text-lg italic text-gray-400">
         {name}
       </h2>
-      <div className="rounded-3xl bg-gray-100 transition-all hover:scale-105 hover:cursor-pointer hover:bg-gray-200">
+      <div
+        className={`rounded-3xl bg-gray-100 transition-all ${getTailwindClassesOnVote(
+          hasCastVote,
+          votedForThisPokemon
+        )}`}
+      >
         <Image
           src={imageUrl}
           alt=""
@@ -96,21 +152,3 @@ function SkeletonPokemonCard() {
 }
 
 export default Home
-
-// const AuthShowcase: React.FC = () => {
-//   const { data: sessionData } = useSession()
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   )
-// }
