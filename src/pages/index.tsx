68bcc89130e54601243a3fadb9654508ced79312
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-03-12 16:00:08 +0800

    feat:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,13 @@ import { type NextPage } from "next"
 import Image from "next/image"
 import Head from "next/head"
 import { trpc } from "../utils/api"
+import DefaultSpinner from "../components/ui/Spinner"
 
 const Home: NextPage = () => {
   const { data, isError, isLoading } =
     trpc.pokemons.getTwoRandomPokemons.useQuery()
   if (isError) {
     return <div>Sorry something went wrong</div>
-  }
-  if (isLoading || !data[0] || !data[1]) {
-    return <div>loading...</div>
   }
   return (
     <>
@@ -21,20 +19,22 @@ const Home: NextPage = () => {
           content="Vote on which pokemon you think is most pointy!"
         />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center gap-16">
-        <h1 className="text-5xl font-extralight text-gray-700">
+      <main className="flex min-h-screen flex-col items-center justify-center sm:gap-16">
+        <h1 className="p-4 text-3xl font-extralight text-gray-700 sm:text-5xl">
           Which pokemon is more <span className="italic">pointy</span>?
         </h1>
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-col gap-8 sm:flex-row">
           <PokemonCard
-            name={data[0].name}
-            imageUrl={data[0].image}
+            name={data?.[0]?.name}
+            imageUrl={data?.[0]?.image}
             isLoading={isLoading}
+            id={data?.[0]?.id}
           />
           <PokemonCard
-            name={data[1].name}
-            imageUrl={data[1].image}
+            name={data?.[1]?.name}
+            imageUrl={data?.[1]?.image}
             isLoading={isLoading}
+            id={data?.[0]?.id}
           />
         </div>
       </main>
@@ -43,17 +43,32 @@ const Home: NextPage = () => {
 }
 
 interface PokemonCardProps {
-  name?: string
-  imageUrl?: string
+  name?: string | undefined
+  imageUrl?: string | undefined
+  id: number | undefined
   isLoading: boolean
 }
 
-function PokemonCard({ name, imageUrl, isLoading }: PokemonCardProps) {
-  if (!imageUrl || isLoading) {
+function PokemonCard({ name, imageUrl, isLoading, id }: PokemonCardProps) {
+  const mutation = trpc.pokemons.voteForPokemonById.useMutation({
+    onSuccess: () => {
+      console.log("we voted!")
+    },
+    onError: (error) => {
+      console.log("We failed :(")
+      console.log(error.message, error.data?.code)
+    }
+  })
+
+  if (!name || !imageUrl || !id || isLoading) {
     return <SkeletonPokemonCard />
   }
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2"
+      onClick={() => mutation.mutate({ id, name, image: imageUrl })}
+    >
       <h2 className="mb-1 flex justify-center text-lg italic text-gray-400">
         {name}
       </h2>
@@ -70,7 +85,14 @@ function PokemonCard({ name, imageUrl, isLoading }: PokemonCardProps) {
 }
 
 function SkeletonPokemonCard() {
-  return <div className="flex h-[400px] w-[400px] bg-gray-200"></div>
+  return (
+    <div className="flex flex-col">
+      <div className="mb-4 flex h-8 w-7/12 animate-pulse self-center rounded-xl bg-gray-200"></div>
+      <div className=" flex h-[400px] w-[400px] animate-pulse items-center justify-center rounded-3xl bg-gray-200 transition-all hover:scale-105 hover:cursor-pointer hover:bg-gray-200">
+        <DefaultSpinner />
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- src/server/api/routers/pokemon.ts
+++ src/server/api/routers/pokemon.ts
@@ -1,12 +1,15 @@
 import { z } from "zod"
 import { createTRPCRouter, publicProcedure } from "../trpc"
+import { TRPCError } from "@trpc/server"
 import { type pokemonAPI } from "../../schemas/pokemon"
 import fetch from "node-fetch"
 import * as _ from "lodash"
+import { prisma } from "../../db"
 
 const pokemonSchema = z.object({
   image: z.string(),
-  name: z.string()
+  name: z.string(),
+  id: z.number()
 })
 
 const getAllPokemonsOutput = pokemonSchema.array()
@@ -23,7 +26,8 @@ export const pokemonRouter = createTRPCRouter({
 
       return res.map((pokemon) => ({
         name: pokemon?.name,
-        image: pokemon.sprites.other?.["official-artwork"]?.front_default
+        image: pokemon?.sprites?.other?.["official-artwork"]?.front_default,
+        id: pokemon?.id
       }))
     }),
   getTwoRandomPokemons: publicProcedure
@@ -36,8 +40,42 @@ export const pokemonRouter = createTRPCRouter({
 
       return res.map((pokemon) => ({
         name: pokemon?.name,
-        image: pokemon.sprites.other?.["official-artwork"]?.front_default
+        image: pokemon.sprites.other?.["official-artwork"]?.front_default,
+        id: pokemon.id
       }))
+    }),
+  voteForPokemonById: publicProcedure
+    .input(pokemonSchema)
+    .output(pokemonSchema)
+    .mutation(async (req) => {
+      const { id, image, name } = req.input
+      if (id < 1 || id > 150)
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Pokemon ids must be between 1 - 150"
+        })
+
+      const pokemon = await prisma.pokemon.findUnique({
+        where: {
+          id: id
+        }
+      })
+
+      if (!pokemon) {
+        return await prisma.pokemon.create({
+          data: {
+            id,
+            image,
+            name,
+            votes: 1
+          }
+        })
+      } else {
+        return await prisma.pokemon.update({
+          where: { id },
+          data: { votes: { increment: 1 } }
+        })
+      }
     })
 })
 
@@ -51,8 +89,12 @@ function getPokemonUrls() {
 
 function getTwoRandomPokemonUrls() {
   const pokemonUrls: string[] = []
-  const firstRandomInt = _.random(1, 150)
+  let firstRandomInt = _.random(1, 150)
   const secondRandomInt = _.random(1, 150)
+
+  while (firstRandomInt === secondRandomInt) {
+    firstRandomInt = _.random(1, 150)
+  }
   pokemonUrls.push(`https://pokeapi.co/api/v2/pokemon/${firstRandomInt}`)
   pokemonUrls.push(`https://pokeapi.co/api/v2/pokemon/${secondRandomInt}`)
   return pokemonUrls
