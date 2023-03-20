16065a4a84fa18b55ea1ada4f251350a8175a037
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-03-20 18:16:51 +0800

    feat: :sparkles: add react-query dev tools to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,24 @@
 import { type AppType } from "next/app"
 import { type Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
-
 import { trpc } from "../utils/api"
-
 import "../styles/globals.css"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps }
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
